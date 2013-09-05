1636675800c7e2b82a373cc5153aa15bd58abe71
3100 <sugar16g@gmail.com>
2013-09-05 08:39:48 +0000

    Solve 005.

---
+++ 005_schedule/3100/lib/scheduling.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+class Scheduling
+  Job = Struct.new('Job', :start, :end)
+  def initialize(n, s, t)
+    @n = n
+    # 終了時刻、開始時刻の順でソートしておく
+    @jobs = s.map.with_index {|start, i|
+      Job.new(start, t[i])
+    }.sort_by!{|job| job.start}.sort_by!{|job| job.end} #逆じゃないよ
+  end
+
+  # 終了時刻の小さなものから貪欲に受注してけば良い
+  def calc
+    last_end = 0
+    count = 0
+    @jobs.each do |job|
+      if job.start > last_end
+        count += 1
+        last_end = job.end
+      end
+    end
+    count
+  end
+end
