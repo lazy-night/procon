b23d018015f35828b376ede913252a5c46c0e0c9
3100 <sugar16g@gmail.com>
2013-09-06 08:35:16 +0000

    Solver 006

---
+++ 006_best_cow_line/3100/spec/cow_spec.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+require_relative '../lib/cow'
+
+describe Cow, '#line_up' do
+  it 'returns ' do
+    input =<<EOF
+6
+A
+C
+D
+B
+C
+B
+EOF
+    cow = Cow.new(input)
+    cow.line_up.should eq('ABCBCD')
+  end
+
+  it 'returns ' do
+    input =<<EOF
+6
+A
+C
+B
+B
+C
+A
+EOF
+    cow = Cow.new(input)
+    cow.line_up.should eq('AACBBC')
+  end
+end
