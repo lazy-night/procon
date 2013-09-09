55d6137b2963698d41ead3515938cdff3c4323d6
3100 <sugar16g@gmail.com>
2013-09-10 06:35:16 +0900

    Modify some bugs and Add a test case.

--- 006_best_cow_line/3100/spec/cow_spec.rb
+++ 006_best_cow_line/3100/spec/cow_spec.rb
@@ -30,4 +30,20 @@ EOF
     cow = Cow.new(input)
     cow.line_up.should eq('AACBBC')
   end
+
+  it 'returns ' do
+    input =<<EOF
+7
+A
+C
+C
+D
+B
+C
+A
+EOF
+    cow = Cow.new(input)
+    cow.line_up.should eq('AACBCCD')
+  end
 end
+
