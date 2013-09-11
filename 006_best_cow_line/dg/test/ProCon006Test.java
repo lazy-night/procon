c6a977e6ab9f67fcd862c164b350f2ecbffeccdb
ryuta koide <koide@ipl.co.jp>
2013-09-11 10:29:16 +0900

    Solve 006 & test

---
+++ 006_best_cow_line/dg/test/ProCon006Test.java
@@ -0,0 +1,78 @@
+package procon;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import lib.StandardInputSnatcher;
+import lib.StandardOutputSnatcher;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProCon006Test {
+	private StandardOutputSnatcher out = new StandardOutputSnatcher();
+	private StandardInputSnatcher in = new StandardInputSnatcher();
+
+	@Before
+	public void before() {
+		System.setOut(out);
+		System.setIn(in);
+	}
+
+	@After
+	public void after() {
+		System.setOut(null);
+		System.setIn(null);
+	}
+	
+	@Test
+	public void proCon006_mainTestData() {
+		proCon006_main_method(
+				6,
+				new char[]{'A', 'C', 'D', 'B', 'C', 'B'},
+				"Old : ACDBCB",
+				"New : ABCBCD");
+		
+		proCon006_main_method(
+				6,
+				new char[]{'A', 'C', 'B', 'B', 'C', 'A'},
+				"Old : ACBBCA",
+				"New : AACBBC");
+		
+		proCon006_main_method(
+				7,
+				new char[]{'A', 'C', 'C', 'D', 'B', 'C', 'A'},
+				"Old : ACCDBCA",
+				"New : AACBCCD");
+		
+		proCon006_main_method(
+				8,
+				new char[]{'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'},
+				"Old : AAAAAAAA",
+				"New : AAAAAAAA");
+	}
+
+	public void proCon006_main_method(int numberOfCow, char[] cowLine, String expectedOld, String expectedNew) {
+		// 入力設定値
+		setInit(numberOfCow, cowLine);
+		
+		// テスト対象の実行
+		ProCon006.main(new String[]{});
+
+		// 出力は改行ごとに取得される
+		String actualOld = out.readLine();
+		String actualNew = out.readLine();
+		assertThat(actualOld, is(expectedOld));
+		assertThat(actualNew, is(expectedNew));
+	}
+	
+	public static void setInit(int numberOfCow, char[] cowLine) {
+		ProCon006.numberOfCow     = numberOfCow;
+		ProCon006.cowLine         = cowLine;
+		ProCon006.arrayCowLine    = new ArrayList<Character>();
+		ProCon006.arrayNewCowLine = new ArrayList<Character>();
+	}
+}
