04dc9e8b7236cb85f421ae9f8eb8f6d2bdf8f35a
3100 <sugar16g@gmail.com>
2013-09-12 05:13:07 +0000

    Improve a spec of 007

--- 007_sarumans_army/3100/spec/solver_spec.rb
+++ 007_sarumans_army/3100/spec/solver_spec.rb
@@ -9,7 +9,7 @@ describe Solver, "#solve" do
     0 3
     10 20 20
     10 7
-    70 30 1 7 15 20 50
+    70 30 2 7 15 20 50
     -1 -1
     EOS1
     2
@@ -28,11 +28,14 @@ describe Solver, "#solve" do
     1 10 5 6 9 13 9 17
     9 10
     70 30 1 7 15 20 50 22 19 18
+    10 3
+    45 58 44
     -1 -1
     EOS1
     5
     2
     4
+    2
     EOS2
 
     solver = Solver.new input
